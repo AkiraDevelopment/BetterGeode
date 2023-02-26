d5149d9790e364856e72de91dbef061378af0b5d
saboooor <saboor547@gmail.com>
2023-02-26 15:56:06 -0700

    fix extension working on all sites

--- background.js
+++ background.js
@@ -1,14 +1,17 @@
-chrome.declarativeContent.onPageChanged.removeRules(null, () => {
-  chrome.declarativeContent.onPageChanged.addRules([{
-    conditions: [
-      new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {
-          hostEquals: 'panel.geode.host'
-        },
-      })
-    ],
-    actions: [
-      new chrome.declarativeContent.ShowPageAction()
-    ]
-  }]);
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.disable();
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {
+            hostSuffix: 'panel.geode.host'
+          },
+        })
+      ],
+      actions: [
+        new chrome.declarativeContent.ShowAction()
+      ],
+    }]);
+  });
 });
